fd29fdcc04104f39f32b0c834be891b74a3d6d28
Maryline Renaud <r.maryline90@gmail.com>
2024-08-10 02:31:27 +0200

    gunterProject

---
+++ Gunter/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import { ref } from 'vue';
+  import Counter from './components/Counter.vue'
+  const toggleGunter = ref(true)
+
+  function showGunter() {
+    toggleGunter.value = !toggleGunter.value;
+  }
+
+</script>
+
+<template>
+  <button @click="showGunter">show BB</button>
+  <section v-if="toggleGunter">
+    <Counter />
+  </section>
+  
+</template>
+
+<style scoped>
+  button{
+    position: absolute;
+    right: 0;
+  }
+</style>
---
+++ Gunter/src/components/Counter.vue
@@ -0,0 +1,61 @@
+<script setup>
+import {ref} from 'vue'
+
+const count = ref(0);
+let colorNumber = true;
+
+function increaseGunter(magicNumber) {
+  if (count.value >= 20) {
+    return;
+  }
+  count.value = count.value + magicNumber
+  colorGunter()
+  if (count.value >= 20){
+    count.value = 20
+  }
+}
+
+function decreaseGunter() {
+  if (count.value === 0){
+    return;
+  }
+  count.value = count.value - 1
+  colorGunter()
+}
+
+function resetGunter() {
+  count.value = 0
+}
+
+function colorGunter() {
+  if (count.value >= 10){
+    colorNumber = false;
+  }
+  else {
+    colorNumber = true;
+  }
+}
+
+</script>
+
+<template>
+  <h1>Mon compteur</h1>
+  <p :style="colorNumber ? 'color: green' : 'color:red'">{{ count }}</p>
+  <button :disabled="count >= 20" @click="increaseGunter(1)">Increase</button>
+  <button @click="decreaseGunter">Decrease</button>
+  <button @click="resetGunter">Reset</button>
+  <button :disabled="count >= 20" @click="increaseGunter(10)">Increase +10</button>
+</template>
+
+<style>
+  button{
+    border-radius: 3em;
+    margin: 1em;
+    padding: 0.5em;
+  }
+
+  p{
+    margin: 1em;
+  }
+
+</style>
